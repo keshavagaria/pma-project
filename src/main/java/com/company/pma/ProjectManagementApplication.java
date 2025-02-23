8dfe02dd8d01e5847046f0e2bd4f44135a28fed3
Vishal Singh <vishal267s@gmail.com>
2025-02-23 17:24:27 +0530

    ok

--- src/main/java/com/company/pma/ProjectManagementApplication.java
+++ src/main/java/com/company/pma/ProjectManagementApplication.java
@@ -2,12 +2,19 @@ package com.company.pma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectManagementApplication {
+public class ProjectManagementApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagementApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(ProjectManagementApplication.class);
+	}
 }
